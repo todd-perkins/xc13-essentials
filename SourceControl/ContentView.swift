4b44554cd0dca9540dcc161d692af5f5a23bec55
Todd Perkins <todd@toddperkins.com>
2022-01-10 21:21:18 -0700

    Resolved conflicts

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world updated fixed bug b!")
+        Text("Hello, world updated fixed bug a and b!")
             .padding()
     }
 }
