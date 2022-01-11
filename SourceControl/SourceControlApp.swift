d67f34ac0a853a14d28d7b835d5e8a41f5ea8466
Todd Perkins <todd@toddperkins.com>
2022-01-10 19:51:18 -0700

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Todd Perkins on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
